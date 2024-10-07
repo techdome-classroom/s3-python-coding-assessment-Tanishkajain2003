43d1da92ca0653e9ada2f0f3bff862eedbca4443
Tanishkajain2003 <147313434+Tanishkajain2003@users.noreply.github.com>
2024-10-07 16:20:41 +0530

    TANISHKA JAIN

--- program1.py
+++ program1.py
@@ -4,6 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        
+        # Initialize the result
+        result = 0
+        
+        # Iterate over the string
+        for i in range(len(s)):
+            # If the current value is less than the next value, subtract it
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                result -= roman[s[i]]
+            else:
+                # Otherwise, add the value
+                result += roman[s[i]]
+                
+        return result
         pass
 
 
